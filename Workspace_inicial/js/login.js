18962159ea5308736dc94ff5c3ea3aec2411ea5a
lulucas200196 <lulucas200196@gmail.com>
2020-08-12 19:51:53 -0300

    Login

--- Workspace_inicial/js/login.js
+++ Workspace_inicial/js/login.js
@@ -1,6 +1,20 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+      function onLogin(){
+    var email = document.getElementById("email").value;
+    var contrasenia = document.getElementById("pass").value;
+    if (email!=""&& contrasenia !="") {
+        window.location='index.html';
+        auxi = 1;
+    }else{        
+        alert("Por favor, complete los campos vacíos");
+    }
+
+    
+};
+
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
